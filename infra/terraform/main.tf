256ac0aa4ea9b5773f3591d7b93dda6d4b15359a
Gisele Cesar <gisele-cesar@hotmail.com>
2025-03-24 00:33:11 -0300

    Ajuste port para mssql

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -35,21 +35,21 @@ resource "aws_security_group" "rds" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 1433
+    to_port     = 1433
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 1433
+    to_port     = 1433
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "sg-rds-fiap-3"
+    name = "sg-rds-fiap-3"
   }
 }
 
