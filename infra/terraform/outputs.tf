367cc654e10744de40063076acce867fba408aa7
Gisele Cesar <gisele-cesar@hotmail.com>
2025-05-18 20:57:15 -0300

    Deploy instancia banco RDS

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -3,18 +3,18 @@
 
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.db-rds-fiap-3.address
+  value       = aws_db_instance.db-rds-fiap-4.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.db-rds-fiap-3.port
+  value       = aws_db_instance.db-rds-fiap-4.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.db-rds-fiap-3.username
+  value       = aws_db_instance.db-rds-fiap-4.username
   sensitive   = true
 }
